139a0e2ad2cd0a5653a28353736d1bc522d90b4a
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-12-29 02:34:20 +0000

    WURFL 2.01 - For XOOPS 2.5 (Only) - Theme Shifter, with Apple and Android Oversight - Heaps of new Features

---
+++ releases/2.01/htdocs/modules/wurfl/language/english/forms.php
@@ -0,0 +1,123 @@
+<?php
+
+	//Device Form
+	define('_WURFL_FRM_FORM_ISNEW_DEVICES', 'New WURFL Device');
+	define('_WURFL_FRM_FORM_ID_DEVICES', 'ID String for Device');
+	define('_WURFL_FRM_FORM_DESC_ID_DEVICES', '');
+	define('_WURFL_FRM_FORM_USERAGENT_DEVICES', 'User Agent For Device');
+	define('_WURFL_FRM_FORM_DESC_USERAGENT_DEVICES', '');
+	define('_WURFL_FRM_FORM_FALLBACK_DEVICES', 'Fallback Device');
+	define('_WURFL_FRM_FORM_DESC_FALLBACK_DEVICES', 'ID String');
+	define('_WURFL_FRM_FORM_MANUFACTURE_DEVICES', 'Manufacture of Device');
+	define('_WURFL_FRM_FORM_DESC_MANUFACTURE_DEVICES', '');
+	define('_WURFL_FRM_FORM_MODEL_DEVICES', 'Model of Device');
+	define('_WURFL_FRM_FORM_DESC_MODEL_DEVICES', '');
+	define('_WURFL_FRM_FORM_SERIES_DEVICES', 'Sub Series of Model of Device');
+	define('_WURFL_FRM_FORM_DESC_SERIES_DEVICES', '');
+	define('_WURFL_FRM_FORM_EDIT_DEVICES', 'Edit Devices');
+		
+	//Form Headers with no Defines
+	define('_WURLF_FRM_CREATE', 'Create');
+	define('_WURLF_FRM_EDIT', 'Edit');
+	define('_WURLF_FRM_UI', 'User Interface');
+	define('_WURLF_FRM_WITHFORM', 'data and information with this form');
+	define('_WURFL_FRM_DEVICE', 'Device Applies To');
+	
+	// File Import Form
+	define('_WURFL_FRM_FILEIMPORT_FORM', 'WURFL XML FILE IMPORT');
+	define('_WURFL_FRM_FILEIMPORT', 'XML File Location');
+	define('_WURFL_FRM_FILEIMPORT_BTTN', 'Import XML File');
+
+	// Dashboard
+	define('_WURFL_FRM_RECORDS', 'Total number of records in database sections!');
+	define('_WURFL_FRM_RECORDS_COUNT_DEVICES', 'Total amount of devices: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_AJAX', 'Devices Supporting AJAX: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_BEARER', 'Number of Bearer Supported Devices: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_BUGS', 'Total Bugs: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_CACHE', 'Amount of Cache Support Records: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_CHTML_UI', 'Total HTML UI Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_CSS', 'Total Records for CSS Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_DISPLAY', 'Total Records for Display Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_DRM', 'Total Records for DRM Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_FLASH_LITE', 'Total Records for Flash lite Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_IMAGE_FORMAT', 'Total Records for Image Format Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_J2ME', 'Total Records for J2ME Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_MARKUP', 'Total Records for Markup Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_MMS', 'Total Records for MMS Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_OBJECT_DOWNLOAD', 'Total Records for Object Download Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_PDF', 'Total Records for PDF Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_PLAYBACK', 'Total Records for Playack Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_PRODUCT_INFO', 'Total Records for Product Information');
+	define('_WURFL_FRM_RECORDS_COUNT_RSS', 'Total Records for RSS Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_SECURITY', 'Total Records for Security Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_SMS', 'Total Records for SMS Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_SOUND_FORMAT', 'Total Records for Sound Format Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_STORAGE', 'Total Records for Storage Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_STREAMING', 'Total Records for Streaming Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_TRANSCODING', 'Total Records for Transcoding Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_WAP_PUSH', 'Total Records for WAP Push Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_WML_UI', 'Total Records for WML UI Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_WTA', 'Total Records for WTA Supported: %s');
+	define('_WURFL_FRM_RECORDS_COUNT_XHTML_UI', 'Total Records for XHTML UI Supported: %s');
+	
+	// Button Creation of Data
+	define('_WURFL_FRM_BUTTON_CREATE_AJAX', 'Create AJAX Support');
+	define('_WURFL_FRM_BUTTON_CREATE_BEARER', 'Create Bearer Support');
+	define('_WURFL_FRM_BUTTON_CREATE_BUGS', 'Create Bugs Support');
+	define('_WURFL_FRM_BUTTON_CREATE_CACHE', 'Create Cache Support');
+	define('_WURFL_FRM_BUTTON_CREATE_CHTML_UI', 'Create HTML UI Support');
+	define('_WURFL_FRM_BUTTON_CREATE_CSS', 'Create CSS Support');
+	define('_WURFL_FRM_BUTTON_CREATE_DISPLAY', 'Create Display Support');
+	define('_WURFL_FRM_BUTTON_CREATE_DRM', 'Create DRM Support');
+	define('_WURFL_FRM_BUTTON_CREATE_FLASH_LITE', 'Create Flash lite Support');
+	define('_WURFL_FRM_BUTTON_CREATE_IMAGE_FORMAT', 'Create Image Format Support');
+	define('_WURFL_FRM_BUTTON_CREATE_J2ME', 'Create J2ME Support');
+	define('_WURFL_FRM_BUTTON_CREATE_MARKUP', 'Create Markup Support');
+	define('_WURFL_FRM_BUTTON_CREATE_MMS', 'Create MMS Support');
+	define('_WURFL_FRM_BUTTON_CREATE_OBJECT_DOWNLOAD', 'Create Object Download Support');
+	define('_WURFL_FRM_BUTTON_CREATE_PDF', 'Create PDF Support');
+	define('_WURFL_FRM_BUTTON_CREATE_PLAYBACK', 'Create Playack Support');
+	define('_WURFL_FRM_BUTTON_CREATE_PRODUCT_INFO', 'Create Product Information');
+	define('_WURFL_FRM_BUTTON_CREATE_RSS', 'Create RSS Support');
+	define('_WURFL_FRM_BUTTON_CREATE_SECURITY', 'Create Security Support');
+	define('_WURFL_FRM_BUTTON_CREATE_SMS', 'Create SMS Support');
+	define('_WURFL_FRM_BUTTON_CREATE_SOUND_FORMAT', 'Create Sound Format Support');
+	define('_WURFL_FRM_BUTTON_CREATE_STORAGE', 'Create Storage Support');
+	define('_WURFL_FRM_BUTTON_CREATE_STREAMING', 'Create Streaming Support');
+	define('_WURFL_FRM_BUTTON_CREATE_TRANSCODING', 'Create Transcoding Support');
+	define('_WURFL_FRM_BUTTON_CREATE_WAP_PUSH', 'Create WAP Push Support');
+	define('_WURFL_FRM_BUTTON_CREATE_WML_UI', 'Create WML UI Support');
+	define('_WURFL_FRM_BUTTON_CREATE_WTA', 'Create WTA Support');
+	define('_WURFL_FRM_BUTTON_CREATE_XHTML_UI', 'Create XHTML UI Support');
+	
+	// Button Edit of Data
+	define('_WURFL_FRM_BUTTON_EDIT_AJAX', 'Edit AJAX Data');
+	define('_WURFL_FRM_BUTTON_EDIT_BEARER', 'Edit Bearer Data');
+	define('_WURFL_FRM_BUTTON_EDIT_BUGS', 'Edit Bugs Data');
+	define('_WURFL_FRM_BUTTON_EDIT_CACHE', 'Edit Cache Data');
+	define('_WURFL_FRM_BUTTON_EDIT_CHTML_UI', 'Edit HTML UI Data');
+	define('_WURFL_FRM_BUTTON_EDIT_CSS', 'Edit CSS Data');
+	define('_WURFL_FRM_BUTTON_EDIT_DISPLAY', 'Edit Display Data');
+	define('_WURFL_FRM_BUTTON_EDIT_DRM', 'Edit DRM Data');
+	define('_WURFL_FRM_BUTTON_EDIT_FLASH_LITE', 'Edit Flash lite Data');
+	define('_WURFL_FRM_BUTTON_EDIT_IMAGE_FORMAT', 'Edit Image Format Data');
+	define('_WURFL_FRM_BUTTON_EDIT_J2ME', 'Edit J2ME Data');
+	define('_WURFL_FRM_BUTTON_EDIT_MARKUP', 'Edit Markup Data');
+	define('_WURFL_FRM_BUTTON_EDIT_MMS', 'Edit MMS Data');
+	define('_WURFL_FRM_BUTTON_EDIT_OBJECT_DOWNLOAD', 'Edit Object Download Data');
+	define('_WURFL_FRM_BUTTON_EDIT_PDF', 'Edit PDF Data');
+	define('_WURFL_FRM_BUTTON_EDIT_PLAYBACK', 'Edit Playack Data');
+	define('_WURFL_FRM_BUTTON_EDIT_PRODUCT_INFO', 'Edit Product Information');
+	define('_WURFL_FRM_BUTTON_EDIT_RSS', 'Edit RSS Data');
+	define('_WURFL_FRM_BUTTON_EDIT_SECURITY', 'Edit Security Data');
+	define('_WURFL_FRM_BUTTON_EDIT_SMS', 'Edit SMS Data');
+	define('_WURFL_FRM_BUTTON_EDIT_SOUND_FORMAT', 'Edit Sound Format Data');
+	define('_WURFL_FRM_BUTTON_EDIT_STORAGE', 'Edit Storage Data');
+	define('_WURFL_FRM_BUTTON_EDIT_STREAMING', 'Edit Streaming Data');
+	define('_WURFL_FRM_BUTTON_EDIT_TRANSCODING', 'Edit Transcoding Data');
+	define('_WURFL_FRM_BUTTON_EDIT_WAP_PUSH', 'Edit WAP Push Data');
+	define('_WURFL_FRM_BUTTON_EDIT_WML_UI', 'Edit WML UI Data');
+	define('_WURFL_FRM_BUTTON_EDIT_WTA', 'Edit WTA Data');
+	define('_WURFL_FRM_BUTTON_EDIT_XHTML_UI', 'Edit XHTML UI Data');
+	
+?>
